a1f86d26786642722faf6a081f7583cb9d9e61af
Venkata Ganesh <venkataganeshdulla9@gmail.com>
2025-07-09 13:46:35 +0530

    Official Voice

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ from app import app, socketio
 if __name__ == '__main__':
     # Note: This part is for local development and will not be used by Gunicorn
     # on Render, but it's good practice to have it.
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
